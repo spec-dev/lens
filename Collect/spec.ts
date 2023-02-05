1ebca1d79754acb16a1a68317b3be1d5adf5334b
Ben Whittle <benwhittle31@gmail.com>
2023-02-04 22:40:30 -0600

    add Collect

---
+++ Collect/spec.ts
@@ -0,0 +1,66 @@
+import { LiveObject, Spec, Property, BlockHash, Address, BlockNumber, Timestamp, ChainId, SpecEvent } from 'https://esm.sh/@spec.dev/core@0.0.19'
+import { OnLensHub } from '../shared/events.ts'
+
+/**
+ * A Lens Collect NFT.
+ */
+@Spec({
+    table: 'lens.collects',
+    uniqueBy: ['profileId', 'pubId', 'collectNftId', 'chainId']
+})
+class Collect extends LiveObject {
+    // The profile token id of the publication being collected.
+    @Property()
+    profileId: number
+
+    // The id of the publication being collected.
+    @Property()
+    pubId: number
+
+    // The token id of the collect NFT.
+    @Property()
+    collectNftId: number
+
+    // The address of the collecter.
+    @Property()
+    collecterAddress: Address
+    
+    // The block timestamp in which the collect NFT was created.
+    @Property({ update: false })
+    createdAt: Timestamp
+
+    // The block hash in which the collect NFT was last updated.
+    @Property()
+    blockHash: BlockHash
+
+    // The block number in which the collect NFT was last updated.
+    @Property()
+    blockNumber: BlockNumber
+
+    // The block timestamp in which the collect NFT was last updated.
+    @Property({ primaryTimestamp: true })
+    blockTimestamp: Timestamp
+
+    // The blockchain id.
+    @Property()
+    chainId: ChainId
+
+    //-----------------------------------------------------
+    //  EVENT HANDLERS
+    //-----------------------------------------------------
+
+    @OnLensHub('CollectNFTTransferred')
+    onCollect(event: SpecEvent) {
+        this.profileId = event.data.profileId
+        this.pubId = event.data.pubId
+        this.collectNftId = event.data.collectNFTId
+        this.collecterAddress = event.data.to
+        this.createdAt = event.data.blockTimestamp
+        this.blockHash = event.data.blockHash
+        this.blockNumber = event.data.blockNumber
+        this.blockTimestamp = event.data.blockTimestamp
+        this.chainId = event.data.chainId
+    }
+}
+
+export default Collect
